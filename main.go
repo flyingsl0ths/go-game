922ee0d1dfe4222f2245d60612e6a1827b0a9f24
username <email>
2023-09-01 11:44:15 -0700

    Add project files

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+func main() {
+	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.DrawText("Hello World!", 342, 450/2, 20, rl.Black)
+
+		rl.EndDrawing()
+	}
+}
