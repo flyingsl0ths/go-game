858fcd175dfd6282e9d54e5e799f66adde6f3f04
flyingsl0ths <randomemail33110@protonmail.com>
2023-09-16 10:12:08 -0700

    Fix typo

--- level.go
+++ level.go
@@ -3,21 +3,21 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func DrawPlatforms(textureAtlas *TextureAtlas, windowDimens [2]float32, textureSize float32) {
-	windowCenter := [2]float32{windowDimens[0] / 2., windowDimens[1]/2. + textureSize}
+	startPosition := [2]float32{windowDimens[0] / 2., windowDimens[1]/2. + textureSize}
 
 	rl.DrawTexturePro(textureAtlas.sheet,
 		textureAtlas.platforms[0],
-		rl.NewRectangle(0., windowCenter[1], textureSize, textureSize),
+		rl.NewRectangle(0., startPosition[1], textureSize, textureSize),
 		rl.NewVector2(0., 0.), 0., rl.White)
 
 	rl.DrawTexturePro(textureAtlas.sheet,
 		textureAtlas.platforms[1],
-		rl.NewRectangle(textureSize, windowCenter[1], textureSize, textureSize),
+		rl.NewRectangle(textureSize, startPosition[1], textureSize, textureSize),
 		rl.NewVector2(0., 0.), 0., rl.White)
 
 	rl.DrawTexturePro(textureAtlas.sheet,
 		textureAtlas.platforms[2],
-		rl.NewRectangle(textureSize*2, windowCenter[1], textureSize, textureSize),
+		rl.NewRectangle(textureSize*2, startPosition[1], textureSize, textureSize),
 		rl.NewVector2(0., 0.), 0., rl.White)
 
 	const currentlyDrawnAtEdge = 3
@@ -26,7 +26,7 @@ func DrawPlatforms(textureAtlas *TextureAtlas, windowDimens [2]float32, textureS
 	for i := 3; i < toDraw; i++ {
 		rl.DrawTexturePro(textureAtlas.sheet,
 			textureAtlas.platforms[3],
-			rl.NewRectangle(textureSize*float32(i), windowCenter[1], textureSize, textureSize),
+			rl.NewRectangle(textureSize*float32(i), startPosition[1], textureSize, textureSize),
 			rl.NewVector2(0., 0.), 0., rl.White)
 	}
 
@@ -34,16 +34,16 @@ func DrawPlatforms(textureAtlas *TextureAtlas, windowDimens [2]float32, textureS
 
 	rl.DrawTexturePro(textureAtlas.sheet,
 		textureAtlas.platforms[total-3],
-		rl.NewRectangle(windowDimens[0]-(textureSize*3), windowCenter[1], textureSize, textureSize),
+		rl.NewRectangle(windowDimens[0]-(textureSize*3), startPosition[1], textureSize, textureSize),
 		rl.NewVector2(0., 0.), 0., rl.White)
 
 	rl.DrawTexturePro(textureAtlas.sheet,
 		textureAtlas.platforms[total-2],
-		rl.NewRectangle(windowDimens[0]-(textureSize*2), windowCenter[1], textureSize, textureSize),
+		rl.NewRectangle(windowDimens[0]-(textureSize*2), startPosition[1], textureSize, textureSize),
 		rl.NewVector2(0., 0.), 0., rl.White)
 
 	rl.DrawTexturePro(textureAtlas.sheet,
 		textureAtlas.platforms[total-1],
-		rl.NewRectangle(windowDimens[0]-textureSize, windowCenter[1], textureSize, textureSize),
+		rl.NewRectangle(windowDimens[0]-textureSize, startPosition[1], textureSize, textureSize),
 		rl.NewVector2(0., 0.), 0., rl.White)
 }
